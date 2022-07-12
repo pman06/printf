05b363474ba84f332d9fe6f0a9de335bbc70b30f
OhiareYazid <isahyazid@yahoo.com>
2022-07-12 09:00:29 -0700

    project completed

--- _printf.c
+++ _printf.c
@@ -1,74 +1,51 @@
-#include <stdio.h>
-#include <stdarg.h>
 #include "main.h"
 
 /**
- * print_func- Function to return function for the
- * function specifier
- * @ls: variadic lists
- * @format: string pointer in printf
- * Return: int
- */
-int print_func(va_list ls, const char *format)
-{
-	int b, i, count;
-	char ch;
-
-	prt arr[] = {
-		{"s", print_string},
-		{"c", print_char},
-		{"d", print_int},
-		{"i", print_int},
-		{NULL, NULL}
-	};
-	count = 0;
-	i = 0;
-	ch = format[i];
-	while (format[i] != '\0')
-	{
-		if (ch == '%')
-		{
-			b = 0;
-			i++;
-			ch = format[i];
-			while (arr[b].c != NULL && ch != *(arr[b].c))
-				b++;
-			if (arr[b].c != NULL)
-				count += arr[b].f(ls);
-			else
-			{
-				if (ch == '\0')
-					return (-1);
-				if (ch == '%')
-					count += _putchar('%');
-				count += _putchar(ch);
-			}
-		}
-		else
-			count += _putchar(format[i]);
-		i++;
-		ch = format[i];
-	}
-	return (count);
-}
-
-
-/**
- * _printf- Function to print anything
- * @format: string to print
- * Return: interger
+ * _printf - formatted output conversion and print data.
+ * @format: input string.
+ *
+ * Return: number of chars printed.
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list ls;
+	unsigned int i = 0, len = 0, ibuf = 0;
+	va_list arguments;
+	int (*function)(va_list, char *, unsigned int);
+	char *buffer;
 
-	if (!format)
-	{
+	va_start(arguments, format), buffer = malloc(sizeof(char) * 1024);
+	if (!format || !buffer || (format[i] == '%' && !format[i + 1]))
 		return (-1);
+	if (!format[i])
+		return (0);
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+			{	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+				return (-1);
+			}
+			else
+			{	function = get_print_func(format, i + 1);
+				if (function == NULL)
+				{
+					if (format[i + 1] == ' ' && !format[i + 2])
+						return (-1);
+					handl_buf(buffer, format[i], ibuf), len++, i--;
+				}
+				else
+				{
+					len += function(arguments, buffer, ibuf);
+					i += ev_print_func(format, i + 1);
+				}
+			} i++;
+		}
+		else
+			handl_buf(buffer, format[i], ibuf), len++;
+		for (ibuf = len; ibuf > 1024; ibuf -= 1024)
+			;
 	}
-	va_start(ls, format);
-	count	+= print_func(ls, format);
-	va_end(ls);
-	return (count);
+	print_buf(buffer, ibuf), free(buffer), va_end(arguments);
+	return (len);
 }
